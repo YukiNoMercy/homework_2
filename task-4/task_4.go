a0ecea16a51cf2c951b8c5c3aec1e40f96f6018f
YukiNoMercy <matveyschenikov@mail.ru>
2023-10-31 23:40:30 +0300

    ready fourth task

--- task-4/task_4.go
+++ task-4/task_4.go
@@ -2,9 +2,11 @@ package main
 
 import "fmt"
 
-func input_matric(size int) [][]int {
+type matric [][]int
+
+func input_matric(size int) matric {
 	var input int
-	matrix := make([][]int, size)
+	matrix := make(matric, size)
 	for i := range matrix {
 		matrix[i] = make([]int, size)
 	}
@@ -19,7 +21,7 @@ func input_matric(size int) [][]int {
 	return matrix
 }
 
-func output_matrix(matrix [][]int, n int) {
+func output_matrix(matrix matric) {
 	for i := 0; i < len(matrix); i++ {
 		for j := 0; j < len(matrix); j++ {
 			fmt.Print(matrix[i][j], " ")
@@ -27,11 +29,25 @@ func output_matrix(matrix [][]int, n int) {
 		fmt.Println()
 	}
 }
+
+func is_simetri(matrix matric) string {
+	for i := 0; i < len(matrix); i++ {
+		for j := 0; j < len(matrix); j++ {
+			if matrix[i][j] != matrix[j][i] {
+				return "no"
+			}
+		}
+	}
+	return "yes"
+}
 func main() {
 	var matrix_size int
 	fmt.Println("Введите размер матрицы")
 	fmt.Scan(&matrix_size)
 	matrix := input_matric(matrix_size)
 
-	output_matrix(matrix, matrix_size)
+	// output_matrix(matrix)
+
+	simetry := is_simetri(matrix)
+	fmt.Println(simetry)
 }
